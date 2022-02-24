916be111c52ee7abc02c829168025dc9c451291a
aenuns23 <aenunsafitri6@gmail.com>
2022-02-24 10:16:31 +0700

    profil sekolah smpn 18

--- app/src/main/java/com/example/profilsekolah2/MainActivity.java
+++ app/src/main/java/com/example/profilsekolah2/MainActivity.java
@@ -33,8 +33,8 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void perpus(View view) {
-        Intent intent = new Intent(MainActivity.this, PerpusActivity.class);
+    public void perpuss(View view) {
+        Intent intent = new Intent(MainActivity.this,PerpusActivity.class);
         startActivity(intent);
     }
 
